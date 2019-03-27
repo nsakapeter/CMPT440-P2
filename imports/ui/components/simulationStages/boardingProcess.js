3f2fdd25e7796022eb0a4e2b582563f4351a37fa
dadada <david.dada@lab.kingsu.ca>
2019-03-26 22:05:18 -0600

    updated zoom & pan

--- imports/ui/components/simulationStages/boardingProcess.js
+++ imports/ui/components/simulationStages/boardingProcess.js
@@ -20,19 +20,33 @@ Template.boardingProcess.onCreated(function() {
 });
 
 Template.boardingProcess.onRendered(function() {
-	// Feel free to change or delete any of the code you see in this editor!
+	width = 900;
+	height = 400;
+	var mycolor = d3.rgb("#4e555b");
+	//var mycolor2 = d3.rgb("#76daff");
+
 	svg = d3.select("#boardingProcess-canvas")
 		.append("svg")
 		.attr("preserveAspectRatio", "xMinYMin meet")
-		.attr("viewBox", "0 0 900 400")
-		.call(d3.zoom().on("zoom", function () {
-			svg.attr("transform", d3.event.transform)
-		}))
+		.attr("viewBox", "0 0 "+width+" "+height)
+		.style("background-color", mycolor)
 		.append("g");
 
+	//d3.select("body").style("background-color", mycolor)
+
+	var zoom = d3.zoom()
+		.scaleExtent([0.5, 8])
+		.translateExtent([[0, 0], [width, height]])
+		.extent([[0, 0], [width, height]])
+		.on("zoom", function () {
+			svg.attr("transform", d3.event.transform)
+		});
+
+	svg.call(zoom);
+
 	svg.append("circle")
-		.attr("cx", document.body.clientWidth / 2)
-		.attr("cy", document.body.clientHeight / 2)
+		.attr("cx", width / 2)
+		.attr("cy", height / 2)
 		.attr("r", 50)
 		.style("fill", "#B8DEE6");
 
@@ -40,8 +54,17 @@ Template.boardingProcess.onRendered(function() {
 		.style("stroke", "gray")
 		.style("fill", "white")
 		.attr("r", 40)
-		.attr("cx", 50)
-		.attr("cy", 50)
+		.attr("cx", 0)
+		.attr("cy", 0)
+		.on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
+		.on("mouseout", function(){d3.select(this).style("fill", "white");});
+
+	svg.append("circle")
+		.style("stroke", "gray")
+		.style("fill", "white")
+		.attr("r", 40)
+		.attr("cx", width)
+		.attr("cy", height)
 		.on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
 		.on("mouseout", function(){d3.select(this).style("fill", "white");});
 });
