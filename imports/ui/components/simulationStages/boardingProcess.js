43df8a3590717e6a1d11a608649d74e2ac441c02
dadada <david.dada@lab.kingsu.ca>
2019-03-31 03:10:22 -0600

    updated passenger queueing, game ticks, passengers dynamically appearing

--- imports/ui/components/simulationStages/boardingProcess.js
+++ imports/ui/components/simulationStages/boardingProcess.js
@@ -20,10 +20,9 @@ Template.boardingProcess.onCreated(function() {
 Template.boardingProcess.onRendered(function() {
     width = document.body.clientWidth;
 //height = 400;
-    var board_x = document.body.clientWidth/2-(document.body.clientWidth/3);
-    var board_y = document.body.clientHeight/2;
+    var board_x = Math.round(document.body.clientWidth/2-(document.body.clientWidth/3)+100);
+    var board_y = Math.round(document.body.clientHeight/2);
     var tickspeed = 1000;
-    var max_visible = 5;
     var rawPassengerData = samplePassengerData(10);
     var data = ParsePassengerData(rawPassengerData);
     var num_passengers = data.length;
@@ -56,30 +55,33 @@ Template.boardingProcess.onRendered(function() {
         .attr("width", "100%")
         .attr("height", "100%");
 
-    /*
-    //draw the same and zoom out
+
+//draw the same and zoom out
     var scale = 80;
     zoom.scaleExtent([(width/100)/(num_passengers*2),1]);
-    */
 
-//draw smaller and zoom in
+
+    /*
+    //draw smaller and zoom in
     var scale = Math.floor(width/(num_passengers*1.4));
     zoom.scaleExtent([1, num_passengers/5]);
-
+    */
 //grouping for circles and text
     var circleGroup = svg.append("g").attr("x",board_x).attr("y",board_y);
 
 
-//initial update attributes
     var domReady = function(callback) {
         document.readyState === "interactive" || document.readyState === "complete" ? callback() : document.addEventListener("DOMContentLoaded", callback);
     };
 
     domReady(function() {
+        //initial update attributes to draw objects to screen
         update();
+        //console.log(data);
+        //set tick speed
+        //setInterval(function(){countDown()},tickspeed);
     });
 
-//update2();
 //start port for passengers
     var dock_x = 0;
     var dock_y = 0;
@@ -93,21 +95,45 @@ Template.boardingProcess.onRendered(function() {
         .style("fill", "#ffc205")
 
 
-//set tick speed
-    setInterval(function(){countDown()},tickspeed);
 
 
-// --------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------- Functions
 
 
     function update() {
-
-//.transition().ease(d3.easeElastic).duration(1000)
+//.transition().ease(d3.easeBounce).duration(1000)
 
         //update circles: select, data, attributes
+
+        /* circleGroup.selectAll("circle").transition().duration(1000).ease(d3.easeElastic)
+             .attr("cx", function(d){return (d.x * scale) + parseInt(circleGroup.attr("x"))-1;})*/
+
         var circles = circleGroup.selectAll("circle")
             .data(data.filter(function(d) { return parseInt(d.visible) === 1; }))
-            .attr("cx", function(d){return (d.x * scale) + parseInt(circleGroup.attr("x"));})
+            .attr("cx", function(d){return ((d.x * scale) + parseInt(circleGroup.attr("x")) );})
             .attr("cy", function(d){return (-d.y * scale) + parseInt(circleGroup.attr("y"));})
             .attr("r", scale/2)
             .attr("wait_current",function(d){return d.wait_current;});
@@ -120,12 +146,14 @@ Template.boardingProcess.onRendered(function() {
             .attr("cy", function(d){return (-d.y * scale) + parseInt(circleGroup.attr("y"));})
             .attr("r", scale/2)
             .attr("wait_current",function(d){return d.wait_current;})
-            .attr("fill", "#B8DEE6")
-            .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
+            .attr("fill", "#B8DEE6");
+
+        circles.on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
             .on("mouseout", function(){d3.select(this).style("fill", "#B8DEE6");});
 
-        //remove circles that dont have corresponding data (filtered out in update)
+        //exit behaviour: remove circles that dont have corresponding data (filtered out in update)
         circles.exit().remove();
+
 
         //update text: select, data, attributes
         var text = circleGroup.selectAll("text")
@@ -149,24 +177,61 @@ Template.boardingProcess.onRendered(function() {
             .attr("font-size", scale/2+"px")
             .attr("fill", "red");
 
+        //exit behaviour: remove text
         text.exit().remove();
+
+
+        /*
+        //Temporary workaround
+        //cx value in contant flux at the 5+ decimal range due the transition
+        var tremor_scale = 1;
+        circles.transition().duration(1000).ease(d3.easeElastic)
+            .attr("cx", function(d){return Math.round(parseInt(d3.select(this).attr("cx")))+tremor_scale})
+            .duration(1000)
+            .transition()
+            .attr("cx", function(d){return Math.round(parseInt(d3.select(this).attr("cx")))})
+            .transition();
+
+        text.transition().duration(1000).ease(d3.easeElastic)
+            .attr("x", function(d){return parseInt(d3.select(this).attr("x"))+tremor_scale})
+            .duration(1000)
+            .transition()
+            .attr("x", function(d){return Math.round(parseInt(d3.select(this).attr("x")))})
+            .transition();
+
+         */
     }
 
-//countdown every game tick: data
+//actions every game tick: data
     function countDown() {
         for(let i=0;i<data.length;i++) {
             var curr_passenger = data[i];
+
+            //set visible and set real walking speed when in front of dock at pos (1,0)
+            if(curr_passenger.x===0) {
+                curr_passenger.wait_reset = curr_passenger.walkingSpeed*10;
+            }
+
+
+            //Before move
+
+
+            //move passengers or countdown
             if(curr_passenger.wait_current>0){
                 curr_passenger.wait_current--;
             } else {
                 if(!adjacentCheck(i)) {
                     curr_passenger.wait_current = curr_passenger.wait_reset;
-                    move(parseInt(curr_passenger.x) + 1, parseInt(curr_passenger.y),i);
+                    move(parseInt(curr_passenger.x)+1, parseInt(curr_passenger.y),i);
                 }
             }
 
-            //set to visible once crossed the starting point to be in view
-            if(curr_passenger.x===0){
+
+            //After move
+
+
+            //if in front of dock at positon (1,0) after move, set visible
+            if (curr_passenger.x===1) {
                 curr_passenger.visible = 1;
             }
         }
@@ -202,7 +267,7 @@ Template.boardingProcess.onRendered(function() {
     }
 
 //generate random passenger data
-    function samplePassengerData(n){
+    function samplePassengerData(n) {
         var passengerList = [];
         for(let i=0;i<n;i++) {
             passengerList.push({
@@ -219,16 +284,15 @@ Template.boardingProcess.onRendered(function() {
 //add new properties to passenger data; x,y,wait_current,wait_reset: uses maxVisable
     function ParsePassengerData(passengerDataRaw) {
         var passengerData = passengerDataRaw;
-
         //set every speed to 1 till objects cross 0,0 so que has no wait time before plane boarding
 
-        //traverse from top to bottom
-        for(let i=0;i<passengerData.length;i++){
+        //traverse from top to bottom, passenger 0 to end
+        for(var i=0;i<passengerData.length;i++){
             //passengers are cued from the -x to 0; 0 being the foremost passenger
             passengerData[i].x = -i;
             passengerData[i].y = 0;
-            passengerData[i].wait_current = passengerData[i].walkingSpeed*10;
-            passengerData[i].wait_reset = passengerData[i].walkingSpeed*10;
+            passengerData[i].wait_current = 0;
+            passengerData[i].wait_reset = 0;
             passengerData[i].visible = 0;
         }
         return passengerData;
